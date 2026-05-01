251fbdee4864a600af4485fbe9aa19ba1c93822f
diogoAraujo-EST <2024149587@estudantes.ips.pt>
2026-05-01 17:24:53 +0100

    Cores, Imagens e Folder Setups
    Adicionado os logos da CERES
    Feito a organização de folders (pode ter mudanças)
    Adicionadas as cores com base nos mockups (tentar não alterar para manter a identidade da aplicação)

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CERESColors {
+  static const Color primaryDarkGreen = Color(0xFF2B5336); // verde escuro dos botões
+  static const Color backgroundCream = Color(0xFFF9F9F6); // Fundo 
+  static const Color textMain = Color(0xFF1E1E1E);
+  static const Color textSecondary = Color(0xFF7A7A7A);
+  static const Color alertOrange = Color(0xFFD9774B); // Cor Alteras
+  static const Color surfaceWhite = Colors.white; // Cartões
+}
